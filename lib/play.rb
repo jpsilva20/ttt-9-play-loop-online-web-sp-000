ff6358944a786299af4d2fc93383c667e1a6027c
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-07 05:32:04 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,14 @@ def turn(board)
 end
 
 # Define your play method below
+counter = 0 
+def play(board)
+  loop do 
+     counter += 1 
+     turn(board)
+    if counter == 9 
+      break
+    end 
+  end
+end 
+
